f03e54436056319f6de7e102043802ea94d5c0be
zabank19 <mr.zabank19@gmail.com>
2025-04-18 18:29:43 +0700

    JavaScript Type Conversion Assignment

--- ex-1.js
+++ ex-1.js
@@ -1,7 +1,7 @@
 let accountBalance = 400;
 let depositAmountFromUser = "10";
 
-accountBalance = accountBalance + depositAmountFromUser;
+accountBalance = accountBalance + Number(depositAmountFromUser);
 
 let accountBalanceMessage = "Your account balance is " + accountBalance;
 
